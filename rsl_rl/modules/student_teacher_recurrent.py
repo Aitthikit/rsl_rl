f907f936acdb9cc13cd69f3c8c9d6cd7966a8d5d
Aitthikit <123627998+Aitthikit@users.noreply.github.com>
2025-10-21 09:33:09 +0700

    Update student_teacher_recurrent.py

--- rsl_rl/modules/student_teacher_recurrent.py
+++ rsl_rl/modules/student_teacher_recurrent.py
@@ -98,3 +98,43 @@ class StudentTeacherRecurrent(StudentTeacher):
         self.memory_s.detach_hidden_states(dones)
         if self.teacher_recurrent:
             self.memory_t.detach_hidden_states(dones)
+    def load_state_dict(self, state_dict, strict=True):
+        """Load the parameters of the student and teacher networks.
+
+        Args:
+            state_dict (dict): State dictionary of the model.
+            strict (bool): Whether to strictly enforce that the keys in state_dict match the keys returned by this
+                           module's state_dict() function.
+
+        Returns:
+            bool: Whether this training resumes a previous training. This flag is used by the `load()` function of
+                  `OnPolicyRunner` to determine how to load further parameters.
+        """
+
+        # check if state_dict contains teacher and student or just teacher parameters
+        if any("actor" in key for key in state_dict.keys()):  # loading parameters from rl training
+            # rename keys to match teacher and remove critic parameters
+            teacher_state_dict = {}
+            for key, value in state_dict.items():
+                if "actor." in key:
+                    teacher_state_dict[key.replace("actor.", "")] = value
+            self.teacher.load_state_dict(teacher_state_dict, strict=strict)
+            # also load recurrent memory if teacher is recurrent
+            if self.teacher_recurrent:
+                memory_t_state_dict = {}
+                for key, value in state_dict.items():
+                    if "memory_a." in key:
+                        memory_t_state_dict[key.replace("memory_a.", "")] = value
+                self.memory_t.load_state_dict(memory_t_state_dict, strict=strict)
+            # set flag for successfully loading the parameters
+            self.loaded_teacher = True
+            self.teacher.eval()
+            return False
+        elif any("student" in key for key in state_dict.keys()):  # loading parameters from distillation training
+            super().load_state_dict(state_dict, strict=strict)
+            # set flag for successfully loading the parameters
+            self.loaded_teacher = True
+            self.teacher.eval()
+            return True
+        else:
+            raise ValueError("state_dict does not contain student or teacher parameters")
